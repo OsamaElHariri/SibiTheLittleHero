0e8392273330b6869a482a64c7287526d75320e0
OsamaElHariri <ose05@mail.aub.edu>
2019-03-02 10:53:50 +0200

    Tests setup

---
+++ tests/sample.test.ts
@@ -0,0 +1,8 @@
+import {expect} from 'chai';
+
+describe('test setup', function() {
+    it('runs a sample test', function() {
+      let result = 4;
+      expect(result).to.be.equal(4);
+    }); 
+  });
