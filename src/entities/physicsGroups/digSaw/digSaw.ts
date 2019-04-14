8a37358fa50798e2d7e1a31545afca3c804a69b9
OsamaElHariri <ose05@mail.aub.edu>
2019-04-14 20:30:10 +0300

    Removed dig saw dig area sprite

--- src/entities/physicsGroups/digSaw/digSaw.ts
+++ src/entities/physicsGroups/digSaw/digSaw.ts
@@ -7,8 +7,8 @@ import { EntityType } from "../entityType";
 
 export class DigSaw extends Phaser.GameObjects.Sprite {
     entityType: EntityType = EntityType.DigSaw;
-    xOriginal:number;
-    yOriginal:number;
+    xOriginal: number;
+    yOriginal: number;
 
     private clockwise: boolean = false;
     private currentDirection: Direction = Direction.Down;
@@ -21,15 +21,6 @@ export class DigSaw extends Phaser.GameObjects.Sprite {
     private accelerationFactor: number = 1.15;
     private rotationDirection: number = -1;
     private maxRotationSpeed: number = 50;
-
-    private bottomDigArea: Phaser.GameObjects.Sprite;
-    private bottomDigAreaOffset: { x: number, y: number };
-    private topDigArea: Phaser.GameObjects.Sprite;
-    private topDigAreaOffset: { x: number, y: number };
-    private rightDigArea: Phaser.GameObjects.Sprite;
-    private rightDigAreaOffset: { x: number, y: number };
-    private leftDigArea: Phaser.GameObjects.Sprite;
-    private leftDigAreaOffset: { x: number, y: number };
 
     private spawnedObjects = [];
     config: DigSawConfigs;
@@ -54,13 +45,7 @@ export class DigSaw extends Phaser.GameObjects.Sprite {
 
         this.scene.add.existing(this);
         this.scene.physics.world.enable(this);
-        this.scene.physics.add.collider(this, platforms,
-            (self: DigSaw, platform: Platform) => {
-                this.bottomDigArea.setMask(platform.spriteMask);
-                this.topDigArea.setMask(platform.spriteMask);
-                this.leftDigArea.setMask(platform.spriteMask);
-                this.rightDigArea.setMask(platform.spriteMask);
-            });
+        this.scene.physics.add.collider(this, platforms);
         this.body.setSize(30, 30);
         this.body.setAllowGravity(false);
         let collider = new FollowCollider(this.scene, this, {
@@ -72,50 +57,6 @@ export class DigSaw extends Phaser.GameObjects.Sprite {
             isUnderGroundHostile: true
         });
         this.spawnedObjects.push(collider);
-
-        this.spawnDigAreas();
-    }
-
-    spawnDigAreas(): void {
-        this.bottomDigArea = this.scene.add.sprite(this.x, this.y, 'DigSawDigArea')
-            .setOrigin(this.clockwise ? 1 : 0, 0.5)
-            .setFlipX(this.clockwise)
-            .setDepth(3);
-
-        this.leftDigArea = this.scene.add.sprite(this.x, this.y, 'DigSawDigArea')
-            .setAngle(90)
-            .setOrigin(this.clockwise ? 1 : 0, 0.5)
-            .setFlipX(this.clockwise)
-            .setDepth(3);
-
-        this.topDigArea = this.scene.add.sprite(this.x, this.y, 'DigSawDigArea')
-            .setAngle(180)
-            .setOrigin(this.clockwise ? 1 : 0, 0.5)
-            .setFlipX(this.clockwise)
-            .setDepth(3);
-
-        this.rightDigArea = this.scene.add.sprite(this.x, this.y, 'DigSawDigArea')
-            .setAngle(-90)
-            .setOrigin(this.clockwise ? 1 : 0, 0.5)
-            .setFlipX(this.clockwise)
-            .setDepth(3);
-
-        if (this.clockwise) {
-            this.bottomDigAreaOffset = { x: 64, y: 42 }
-            this.leftDigAreaOffset = { x: -42, y: -64 }
-            this.topDigAreaOffset = { x: 64, y: -42 }
-            this.rightDigAreaOffset = { x: 42, y: -64 }
-        } else {
-            this.bottomDigAreaOffset = { x: -64, y: 42 }
-            this.leftDigAreaOffset = { x: -42, y: -64 }
-            this.topDigAreaOffset = { x: 64, y: -42 }
-            this.rightDigAreaOffset = { x: 42, y: 64 }
-        }
-
-        this.spawnedObjects.push(this.bottomDigArea,
-            this.leftDigArea,
-            this.topDigArea,
-            this.rightDigArea);
     }
 
     update(): void {
@@ -130,28 +71,6 @@ export class DigSaw extends Phaser.GameObjects.Sprite {
         let previous: Direction = this.previousDirection(this.currentDirection);
         this.applySpeedInDirection(this.currentSpeed, this.currentDirection);
         this.applySpeedInDirection(2, previous);
-
-
-        this.bottomDigArea
-            .setPosition(this.x + this.bottomDigAreaOffset.x, this.y + this.bottomDigAreaOffset.y)
-            .setAlpha(previous == Direction.Down && this.body.blocked.down ? 1 : 0)
-            .setScale(1 + speedFraction / 2, 1);
-
-        this.leftDigArea
-            .setPosition(this.x + this.leftDigAreaOffset.x, this.y + this.leftDigAreaOffset.y)
-            .setAlpha(previous == Direction.Left && this.body.blocked.left ? 1 : 0)
-            .setScale(1 + speedFraction / 2, 1);
-
-        this.topDigArea
-            .setPosition(this.x + this.topDigAreaOffset.x, this.y + this.topDigAreaOffset.y)
-            .setAlpha(previous == Direction.Up && this.body.blocked.up ? 1 : 0)
-            .setScale(1 + speedFraction / 2, 1);
-
-        this.rightDigArea
-            .setPosition(this.x + this.rightDigAreaOffset.x, this.y + this.rightDigAreaOffset.y)
-            .setAlpha(previous == Direction.Right && this.body.blocked.right ? 1 : 0)
-            .setScale(1 + speedFraction / 2, 1);
-
     }
 
     setDirection(): void {
