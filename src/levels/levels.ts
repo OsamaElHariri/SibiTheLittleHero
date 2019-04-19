966efd3fdcc0433e0ae0d19bf3b2e3cdc169aaf3
OsamaElHariri <ose05@mail.aub.edu>
2019-04-19 10:24:39 +0300

    Level 4

--- src/levels/levels.ts
+++ src/levels/levels.ts
@@ -1,7 +1,7 @@
 import * as level1 from "../levels/Level1/level1_v5.json";
 import * as level2 from "../levels/Level2/level2_v3.json";
 import * as level3 from "../levels/Level3/level3_v5.json";
-import * as level4 from "../levels/Level4/level4_v2.json";
+import * as level4 from "../levels/Level4/level4_v4.json";
 
 export let levels = [
     level1,
